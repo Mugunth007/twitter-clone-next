eb752a7a4815893d4903c36cd417f79500aa6b6b
MugunthPerumal <81457130+Mugunth007@users.noreply.github.com>
2023-05-10 08:15:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Resolve the '@' alias to the root directory
+    config.resolve.alias['@'] = path.join(__dirname, '');
+
+    return config;
+  },
+};
+
 module.exports = nextConfig
